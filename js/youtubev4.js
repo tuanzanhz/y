3d286f3082da135e316e01075c92fc39620b6e53
tuanzanhz <109707658+tuanzanhz@users.noreply.github.com>
2022-11-03 07:52:49 +0700

    Update and rename youtubev3.js to youtubev4.js

--- js/youtubev4.js
+++ js/youtubev4.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     var inputSearch = document.getElementById("keyword");
-    inputSearch.onkeydown = function(event) {
+    inputSearch.onkeydown = function(event){
         if (event.keyCode == 13){
             loadVideo(this.value);
         }
     }
-    loadVideo("Đen vâu");
+    loadVideo("BigBang");
 });
 
 var modal = document.getElementById('myModal');
@@ -18,36 +18,36 @@ span.onclick = function() {
     closeVideo();
 }
 
-window.onclick = function(event) {
+window.onclick = function(event){
     if (event.target == modal) {
         closeVideo();
     }
 }
 
 function loadVideo(keyword){
-    var YOUTUBE_API ="https://content.googleapis.com/youtube/v3/search?q=" + keyword + "&type=video&maxResults=9&part=snippet&key=AIzaSyAPTn0w8OL92ueAFZsnswHLWuT4lLEOXiE";
+    var YOUTUBE_API ="https://content.googleapis.com/youtube/v3/search?q=" + keyword + "&type=video&maxResults=9&part=snippet&key=AIzaSyDaR_U9ruoA-BLSaGBNyf4DTPGxAixftXA";
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", YOUTUBE_API, true);
+    xhr.open("GET",YOUTUBE_API, true);
     xhr.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             var responseJson = JSON.parse(this.responseText);
             var htmlContent = "";
-            for (var i = 0; i < responseJson.items.length;i++){
+
+            for (var i=0; i < responseJson.items.length;i++){
                 if(responseJson.items[i].id.kind == 'youtube#channel'){
                     continue;
                 }
                 var videoId = responseJson.items[i].id.videoId;
                 var videoTitle = responseJson.items[i].snippet.title;
-                var videoDescription = responseJson.items[i].snippet.description;
+                var videoDescripttion = responseJson.items[i].snippet.description;
                 var videoThumbnail = responseJson.items[i].snippet.thumbnails.medium.url;
                 htmlContent += '<div class="video" onclick="showVideo(\'' + videoId + '\')">'
-                    htmlContent += '<img src=" ' + videoThumbnail + '">'
-                    htmlContent += '<div class="title">' + videoTitle + '</div>'
+                htmlContent += '<img src="' + videoThumbnail + '">'
+                htmlContent += '<div class="title">' +  videoTitle + '</div>'
                 htmlContent += '</div>'
             }
-
-            document.getElementById("list-video").innerHTML= htmlContent;
-        } else if (this.readyState == 4) {
+            document.getElementById("list-video").innerHTML=htmlContent;
+        }else if(this.readyState==4){
             console.log("Fails");
         }
     };
@@ -56,11 +56,11 @@ function loadVideo(keyword){
 
 function closeVideo(){
     modal.style.display = "none";
-        videoFrame.src = "";
+    videoFrame.src ="";
 }
 
 function showVideo(videoId){
-    videoFrame.src ="https://www.youtube.com/embed/" + videoId + "?autoplay=1";
+    videoFrame.src = "https://www.youtube.com/embed/" + videoId + "?autoplay=1";
     setTimeout(function(){
         modal.style.display = "block";
     }, 300);
